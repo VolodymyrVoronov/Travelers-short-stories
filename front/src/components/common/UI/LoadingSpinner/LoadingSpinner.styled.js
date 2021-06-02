40a6217a5914169b8bc64f00259ff03251faf672
Volodymyr Voronov <malones.vladimir@gmail.com>
2021-06-02 20:02:24 +0200

    styles(LoadingSpinner.styled.js): added media queries

--- front/src/components/common/UI/LoadingSpinner/LoadingSpinner.styled.js
+++ front/src/components/common/UI/LoadingSpinner/LoadingSpinner.styled.js
@@ -1,10 +1,24 @@
 import styled from "styled-components";
 
+import { device } from "./../../../../styles/breakPoints";
+
 const LoadingSpinnerContainer = styled.div`
   display: flex;
   justify-content: center;
 
   margin-top: 35px;
+
+  @media ${device.mobileS} {
+    margin-top: 25px;
+  }
+
+  @media ${device.mobileM} {
+    margin-top: 30px;
+  }
+
+  @media ${device.mobileL} {
+    margin-top: 35px;
+  }
 `;
 
 export { LoadingSpinnerContainer };
